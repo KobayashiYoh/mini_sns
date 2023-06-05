f229fba7e8195eb33e80db99474cc5a8361a9835
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-06 00:35:44 +0900

    サインアップ後にタイムラインへ遷移

--- timeline/views.py
+++ timeline/views.py
@@ -9,6 +9,7 @@ def signup_view(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('post_list')
     else:
         form = SignupForm()
     param = {
