3d9c1b18d9dd6b947e1d84d3c06d3838a8bac3d7
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-05 19:07:30 +0900

    post_edit.htmlを追加して新規投稿機能を追加

--- timeline/views.py
+++ timeline/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.all()
@@ -8,3 +10,15 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'timeline/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'timeline/post_edit.html', {'form': form})
